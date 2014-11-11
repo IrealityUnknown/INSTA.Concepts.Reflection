f9e2c7eacdd1f7285e89524463e179f2d7a22bda
Nicolas DIDIER <ireality@free.fr>
2014-11-11 18:55:32 +0100

    First Commit

---
+++ INSTA.Concepts.Reflexion/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSTA.Concepts.Reflexion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test_Blab();
+
+            Test_GetProperty();
+        }
+
+        private static void Test_Blab()
+        {
+            Type type = typeof (StringBuilder);
+
+            // On fait imprimer la liste de toutes les méthodes
+            // de l'objet Type en cours
+            Console.WriteLine("##### Methods ##### {0}", "\r\n");
+            foreach (var m in type.GetMethods())
+            {
+                Console.WriteLine(m.Name);
+            }
+            Console.WriteLine();
+
+            // On fait imprimer la liste de toutes les méthodes
+            // de l'objet Type en cours
+            Console.WriteLine("##### Contructors ##### {0}", "\r\n");
+            foreach (var m in type.GetConstructors())
+            {
+                Console.WriteLine(m.Name);
+                foreach (var param in m.GetParameters())
+                {
+                    Console.WriteLine("\t({1} : {2})", m.Name, param.ParameterType.Name, param.Name);
+                }
+            }
+            Console.WriteLine();
+
+            // On fait imprimer la liste de toutes les propriétés
+            // de l'objet Type en cours
+            Console.WriteLine("##### Properties ##### {0}", "\r\n");
+
+            foreach (var m in type.GetProperties())
+            {
+                Console.WriteLine("{0} : {1}", m.Name, m.PropertyType.Name);
+            }
+            Console.WriteLine();
+        }
+
+
+        public static void Test_GetProperty()
+        {
+            var p = new Person() {Id = 15};
+
+            var result = (int) ReflectionHelper.GetPropertyValue(p, "Id");
+
+            Console.WriteLine("The Id of the Person : {0}", result);
+        }
+    }
+
+}
---
+++ INSTA.Concepts.Reflexion/ReflectionHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSTA.Concepts.Reflexion
+{
+    /// <summary>
+    /// Classe facilitant les opérations de réflxion..
+    /// </summary>
+    public class ReflectionHelper
+    {
+        /// <summary>
+        /// Gets the standard bindings (public + non public + static + instance).
+        /// </summary>
+        /// <returns>
+        ///     A value of type BindingFlags that specifies ALL members.
+        /// </returns>
+        public static BindingFlags GetStandardBindings ()
+        {
+            return BindingFlags.Public |
+                   BindingFlags.NonPublic |
+                   BindingFlags.Instance |
+                   BindingFlags.Static;
+
+        }
+
+        public static object GetPropertyValue(object container, string propertyName)
+        {
+            // On obtient le type de l'objet contenant la propriété
+            var containerType = container.GetType();
+
+            // On obtient une référence vers l'objet PropertyInfo
+            // qui représente notre propriété..
+            //var property = containerType.GetProperty(propertyName, GetStandardBindings());
+
+            var result = containerType.InvokeMember(propertyName,
+                GetStandardBindings() | BindingFlags.GetProperty,
+                null,
+                container,
+                null);
+
+            return result;
+        }
+    }
+}
